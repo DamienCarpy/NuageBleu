e29b823972a623b33e0ae8b387d7b3f0e12cd6c1
DamienCarpy <damien.carpy@devoteam.com>
2021-11-26 15:55:46 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+    }
+  }
+}
+
+provider "ibm" {
+  # visibility       = "private"
+  region           = "bnpp_eu"
+  ibmcloud_api_key = var.ibmcloud_api_key
+}
+
+resource "ibm_resource_group" "ResourceGroup" {
+  name = "TestDca"
+}
+
